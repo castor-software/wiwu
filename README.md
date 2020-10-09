70f01b1a178853d9adba04a1e9d3a4cafde675a7
cesarsotovalero <cesarsotovalero@gmail.com>
2020-10-09 12:25:23 +0200

    Checkout an specific tag

---
+++ README.md
@@ -0,0 +1,6 @@
+# wiwu
+
+Wiwu analyzes the evolution of code bloat in Java projects throughout its git history.
+
+
+ 
--- src/main/java/se/kth/castor/App.java
+++ src/main/java/se/kth/castor/App.java
@@ -1,6 +1,7 @@
 package se.kth.castor;
 
 import org.apache.commons.io.FileUtils;
+import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 
 import java.io.File;
@@ -8,21 +9,32 @@ import java.io.IOException;
 
 public class App {
     private static final String repositoryName = "jcabi/jcabi-github";
-    private static File clonedRepositoryDir = new File(
-            "cloned-repository" + "/" + repositoryName.split("/")[1]
+    private static final File clonedRepositoryDir = new File(
+            "/src/main/resources/cloned-repository" + "/" + repositoryName.split("/")[1]
     );
 
     public static void main(String[] args) throws IOException, GitAPIException {
-
         GitHubStats gitHubStats = new GitHubStats(repositoryName);
-        RemoteRepository remoteRepository = remoteRepository(repositoryName, gitHubStats);
+        RemoteRepository remoteRepository;
 
         if (!clonedRepositoryDir.exists()) {
             remoteRepository = remoteRepository(repositoryName, gitHubStats);
         }
 
+        Git git = Git.open(clonedRepositoryDir);
+
+        git.tagList()
+                .call()
+                .forEach(a -> System.out.println(a.getName()));
+
+        git.tagList()
+                .call()
+                .forEach(a -> System.out.println(a.getObjectId()));
+
         // TODO checkout tag on the tag and apply DepClean there
-        System.out.println(remoteRepository.tagNames());
+        git.checkout()
+                .setName("0.11.1")
+                .call();
     }
 
     private static RemoteRepository remoteRepository(String repositoryName, GitHubStats gitHubStats)
